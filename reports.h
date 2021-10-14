bbcce276b854993c8cdfe6b72c912b7a287ce673
dbmartinez <dbmartinez@me.com>
2021-10-13 21:24:14 -0700

    add reports.h file

---
+++ reports.h
@@ -0,0 +1,17 @@
+// reports.h
+
+#ifndef REPORTS_H
+#define REPORTS_H
+
+class Reports
+{
+	void reports();
+	void repListing();
+	void repWholesale();
+	void repRetail();
+	void repQty();
+	void repCost();
+	void repAge();
+
+};
+#endif
